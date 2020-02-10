6d92b8b4114d87f76ae9da762e959dfed53a9232
Michael Redig <mredig@gmail.com>
2020-02-10 01:44:55 -0600

    renamed package

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Auth2Learn",
+    name: "SimpleAuthStarter",
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
